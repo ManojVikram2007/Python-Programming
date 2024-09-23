8ff42f1f9c477ba6373ee6105366d8435a0047ea
ManojVikram2007 <manojvikram2007@gmail.com>
2024-09-23 20:34:55 +0530

    Add files via upload

---
+++ automorphic.py
@@ -0,0 +1,21 @@
+def func():
+    number=int(input("Enter a Number:"))
+
+    square=number*number
+    temp=square%10
+    if(number%10==temp)&(squar10==):
+        print("Automorphic Number")
+    else:
+        print("Not an automorphic number")
+func()
+
+def func1(num):
+    square=num*num
+    temp=square%10
+    if(num%10==temp):
+        print("Automorphic Number")
+    else:
+        print("Not an Automorphic Number")
+number=int(input("Enter The Number"))
+func1(number)
+
